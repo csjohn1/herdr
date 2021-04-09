fdd32257f5b33c7215640a32fef6a0cecabfa6fa
Chris John <csjohn@shaw.ca>
2021-04-09 14:17:49 -0400

    adding home button functionality to logo and title

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import goat from "../../assets/goat.png";
 import arrow from "../../assets/arrow.svg";
+import { ChatContext } from "../../context/chat";
 import "./topbar.css";
 
 export function TopBar(props) {
+  const { chatDispatch } = useContext(ChatContext);
+
   return (
     <div className="topbar">
-      <div className="top-title-logo">
+      <div
+        className="top-title-logo"
+        onClick={() => {
+          chatDispatch({
+            type: "close_chat",
+          });
+        }}
+      >
         <img src={goat} height="45px" alt="goat" />
         <div className="top-title">herdr</div>
       </div>
