f269d97bd0d1efaf73ed44dcb05b6b09cd3cf074
Chris John <csjohn@shaw.ca>
2021-04-09 13:47:32 -0400

    commenting out swipe buttons

--- src/components/image_card/image_card.js
+++ src/components/image_card/image_card.js
@@ -4,10 +4,16 @@ import { store } from "../../context/cards.js";
 import TinderCard from "react-tinder-card";
 import "./image_card.css";
 
+
+
 export function ImageCard(props) {
   const { matchDispatch } = useContext(match);
   const { state, cardDispatch } = useContext(store);
   const characters = state.cards;
+
+  // const alreadyRemoved = []
+
+  // const childRefs = useMemo(() => Array(characters.length).fill(0).map(i => React.createRef()), [])
 
   const swiped = (direction, character) => {
     if (direction === "right" && character.match) {
@@ -16,13 +22,24 @@ export function ImageCard(props) {
     }
     cardDispatch({ type: "REMOVE", character });
   };
+
+  // const swipe = (dir) => {
+  //   const cardsLeft = characters.filter(person => !alreadyRemoved.includes(person.name))
+  //   if (cardsLeft.length) {
+  //     const toBeRemoved = cardsLeft[cardsLeft.length - 1].name
+  //   const index = characters.map(person => person.name).indexOf(toBeRemoved)
+  //   childRefs[index].current.swipe(dir) // Swipe the card!
+  //   }
+  // }
   
   return (
+    <>
     <div className="image-card-container">
-      {characters.map((character) => (
+      {characters.map((character, index) => (
         <TinderCard
           className="swipe"
           key={character.name}
+          // ref={childRefs[index]}
           onSwipe={(dir) => swiped(dir, character)}
         >
           <div
@@ -34,5 +51,7 @@ export function ImageCard(props) {
         </TinderCard>
       ))}
     </div>
+     
+    </>
   );
 }
